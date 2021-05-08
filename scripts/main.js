c6bf951661da420a570edc89557681925f9a626e
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-05-08 16:56:43 +0200

    created checkbox function

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 function updateCounters() {
   // Total number of todos
   const totalCount = document.getElementById("total-count");
+  console.log("totalCount ", totalCount);
   const totalTodos = document.getElementsByClassName("todo").length;
   totalCount.innerHTML = totalTodos;
+  console.log("totalCount ", totalCount);
 
   // Total number of completed todos
   const completedCount = document.getElementById("completed-count");
@@ -16,3 +18,22 @@ function updateCounters() {
 }
 
 updateCounters();
+
+function toggleDone(event) {
+  const checkbox = event.currentTarget;
+  // check the checked status of the checkbox
+  if (checkbox.checked) {
+    // the "completed" class is set on the parent element, the <li>
+    checkbox.parentElement.parentElement.className = "todo completed";
+  } else {
+    checkbox.parentElement.parentElement.className = "todo";
+  }
+
+  updateCounters();
+}
+
+const checkboxes = document.querySelectorAll(".todo input");
+
+for (let i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener("change", toggleDone);
+}
