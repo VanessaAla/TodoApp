6886b48b664889e2a898596e6eb00aa980f5a45d
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-05-10 15:24:18 +0200

    created new todo function

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,8 @@
 function updateCounters() {
   // Total number of todos
   const totalCount = document.getElementById("total-count");
-  console.log("totalCount ", totalCount);
   const totalTodos = document.getElementsByClassName("todo").length;
   totalCount.innerHTML = totalTodos;
-  console.log("totalCount ", totalCount);
 
   // Total number of completed todos
   const completedCount = document.getElementById("completed-count");
@@ -37,3 +35,48 @@ const checkboxes = document.querySelectorAll(".todo input");
 for (let i = 0; i < checkboxes.length; i++) {
   checkboxes[i].addEventListener("change", toggleDone);
 }
+
+function createTodo(title) {
+  // create a label
+  const label = document.createElement("label");
+
+  // create a checkbox
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = false;
+  // add the "change" event listener to the checkbox
+  checkbox.addEventListener("change", toggleDone);
+  // and add the checkbox to the label
+  label.appendChild(checkbox);
+
+  // create a text node with the given title
+  const labelText = document.createTextNode(" " + title);
+  // and add the text node to the label
+  label.appendChild(labelText);
+
+  // create a list item
+  const listItem = document.createElement("li");
+  listItem.className = "todo";
+  // and add the label to it
+  listItem.appendChild(label);
+
+  // add the list item to the todo list
+  const list = document.getElementById("todolist");
+  list.appendChild(listItem);
+}
+
+document
+  .querySelector("form")
+  .addEventListener("submit", function addNewTodo(event) {
+    event.preventDefault();
+
+    const inputField = document.querySelector("#new-todo");
+    const newTodoTitle = inputField.value;
+    createTodo(newTodoTitle);
+
+    // reset the value of the inputField to make it empty and
+    // ready to create new todos
+    inputField.value = null;
+
+    updateCounters();
+  });
